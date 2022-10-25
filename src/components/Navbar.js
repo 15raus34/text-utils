2796118cd9f845d1ba6a4343a5de0a649d973407
Raus <rausaray1234@gmail.com>
2022-10-25 23:42:53 +0545

    CORRECTED HREF TO A

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,9 +22,9 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <href className="nav-link active" aria-current="page" to="/">
+                <a className="nav-link active" aria-current="page" href="/">
                   Home
-                </href>
+                </a>
               </li>
             </ul>
             <div className="form-check form-switch mx-3">
